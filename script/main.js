160d56c8408aea1ad9dbb04e7f8088fcb09ad1b7
Kirill Sheyanov <amocodingsystem@gmail.com>
2024-04-23 02:57:52 +0300

    Добавлены файлы. И сделан рандом 4 героев

--- script/main.js
+++ script/main.js
@@ -1,14 +1,43 @@
-let heroesQuantity = 124;
+let heroesQuantity = 4;
 
 let heroInfo = {
-   id: 0,
-   name: "",
-   image: "",
+   id: 1,
+   name: "Abaddon",
+   image: "alchemist.png",
+   count: 1,
 };
+
+let wrapperClass = document.querySelector(".wrapper");
+let contentClass = wrapperClass.querySelector(".content");
+let heroClass = contentClass.querySelector(".choose-button");
+let heroImage = contentClass.querySelector(".hero-image");
+
+heroClass.addEventListener("click", heroesRandom);
 
 function heroesRandom() {
    let randId = Math.ceil(Math.random() * heroesQuantity);
+   if (randId === 1) {
+      heroImage.setAttribute(
+         "style",
+         "background-image: url(../images/abaddon.png)"
+      );
+   } else if (randId === 2) {
+      heroImage.setAttribute(
+         "style",
+         "background-image: url(../images/alchemist.png)"
+      );
+   } else if (randId === 3) {
+      heroImage.setAttribute(
+         "style",
+         "background-image: url(../images/ancient_apparition.png)"
+      );
+   } else if (randId === 4) {
+      heroImage.setAttribute(
+         "style",
+         "background-image: url(../images/antimage.png)"
+      );
+   }
    console.log("Номер " + randId);
 }
 
-heroesRandom();
+console.log(heroClass.className);
